223aa8f2a73c64cd9af997d0889a6478972bcc7c
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-12-16 20:59:57 +0100

    Add routes for MovieDetailsPage, TvShowDetailsPage, ActorDetailsPage, and ProfilePage in GoRouter

--- lib/views/router/go_router.dart
+++ lib/views/router/go_router.dart
@@ -1,21 +1,36 @@
 import 'package:go_router/go_router.dart';
 import 'package:moviestack/views/pages/index.dart'
-    show SplashPage, PopularNowPage, SearchPage;
+    show
+        SplashPage,
+        PopularNowPage,
+        SearchPage,
+        MovieDetailsPage,
+        TvShowDetailsPage,
+        ActorDetailsPage,
+        ProfilePage;
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
+    GoRoute(path: '/', builder: (context, state) => SplashPage()),
     GoRoute(
-      path: '/',
-      builder: (context, state) => SplashPage(),
+        path: '/popular-now', builder: (context, state) => PopularNowPage()),
+    GoRoute(path: '/search', builder: (context, state) => SearchPage()),
+    GoRoute(
+      path: '/movie/:id',
+      builder: (context, state) =>
+          MovieDetailsPage(movieId: int.parse(state.pathParameters['id']!)),
     ),
     GoRoute(
-      path: '/popular-now',
-      builder: (context, state) => PopularNowPage(),
+      path: '/tv-show/:id',
+      builder: (context, state) =>
+          TvShowDetailsPage(tvShowId: int.parse(state.pathParameters['id']!)),
     ),
     GoRoute(
-      path: '/search',
-      builder: (context, state) => SearchPage(),
+      path: '/actor/:id',
+      builder: (context, state) =>
+          ActorDetailsPage(actorId: int.parse(state.pathParameters['id']!)),
     ),
+    GoRoute(path: '/profile', builder: (context, state) => ProfilePage()),
   ],
 );
