c58e3801431fd1321ae04ed87f00c323e19e9ff5
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-12-16 17:02:55 +0100

    Add SplashPage with logo display and 3-second delay

---
+++ lib/views/pages/splash_page/splash_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:moviestack/views/index.dart' show Logo; // Import Logo widget
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Add a delay of 3 seconds before checking the login status
+    Future.delayed(const Duration(seconds: 3), () async {});
+
+    // Display the splash screen with the logo
+    return const Scaffold(
+      body: Center(
+        child: Logo(
+          size: 50,
+        ), // Show logo for splash screen
+      ),
+    );
+  }
+}
